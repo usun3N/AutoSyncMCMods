5c083ec24907bb863dd6fcdeb0289aaf55bde7ad
BuildTools <unconfigured@null.spigotmc.org>
2023-11-24 18:41:55 +0900

    fix request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import glob
 from os.path import exists
+from os import mkdir
 import pyperclip
 import requests
 
@@ -24,14 +25,21 @@ def save_option():
 def load_profiles():
     for path in profiles_path:
         with open(f"{path}/info.txt") as f:
-            profile_id, profile_name, modpack_version, info_url = f.read().split("\n")
+            profile_id, profile_name, info_url, modpack_version = f.read().split("\n")
             profiles_dict[profile_id] = {"name" : profile_name, "version" : float(modpack_version), "url" : info_url}
 
 
 def add_profile(url):
     data = requests.get(url).content.decode()
-    print(str((data)).split("\n"))
+    r_id, r_name, r_url, r_version = str((data)).split("\n")
 
+    if exists(f"./profiles/{r_id}/info.txt"):
+        return False
+    
+    mkdir(f"./profiles/{r_id}")
+    with open(f"./profiles/{r_id}/info.txt", "w") as f:
+        f.write(data)
+    return True
 
 def main():
     global directory_path, selected_profile
@@ -46,7 +54,8 @@ def main():
 
     add_profile_tab = [[sg.Text("URL")],
                        [sg.InputText(key="-add_profile_url-", size=40), sg.Button("貼り付け", key="-add_profile_url_paste-")],
-                       [sg.Button("追加", key="-add_profile_accept-", disabled=False)]]
+                       [sg.Button("追加", key="-add_profile_accept-", disabled=False)],
+                       [sg.Text("", key="-add_profile_state-")]]
 
     option_tab = [[sg.Text("起動構成用のディレクトリ")],
                   [sg.InputText(key='-option_directory_path-', size=40, default_text=directory_path), sg.Button("貼り付け", key='-option_directory_path_paste-')]]
@@ -72,7 +81,12 @@ def main():
         
         elif event =="-add_profile_accept-":
             window["-add_profile_accept-"].update(disabled=True)
-            add_profile(values["-add_profile_url-"])
+            window["-add_profile_state-"].update("Processing...")
+            if add_profile(values["-add_profile_url-"]):
+                window["-add_profile_state-"].update("Success!")
+            else:
+                window["-add_profile_state-"].update("Error")
+
     
     save_option()
     window.close()
