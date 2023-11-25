5107c8df7c74bb1fbfbe39b9d1fc237a61129810
BuildTools <unconfigured@null.spigotmc.org>
2023-11-25 20:23:55 +0900

    add delete profile

--- main.py
+++ main.py
@@ -55,19 +55,19 @@ def unpack_file(path, out):
     remove(path)
 
 
-def add_profile_thread(url, r_id, key):
+def add_profile_thread(url, r_id):
     try:
-        window[key].update("Downloading...")
+        window["-add_profile_state-"].update("Downloading...")
         download_file(url, f"./profiles/{r_id}/mods.zip")
-        window[key].update("Making Directory...")
+        window["-add_profile_state-"].update("Making Directory...")
         makedirs(f"{directory_path}/{r_id}/mods")
-        window[key].update("Unpacking...")
+        window["-add_profile_state-"].update("Unpacking...")
         unpack_file(f"./profiles/{r_id}/mods.zip", f"{directory_path}/{r_id}/mods")  
-        window[key].update("Profiles Reloading...")
+        window["-add_profile_state-"].update("Profiles Reloading...")
         load_profiles()
-        window[key].update("Success!")
+        window["-add_profile_state-"].update("Success!")
     except:
-        window[key].update("Failed")
+        window["-add_profile_state-"].update("Failed")
     finally:
         window["-add_profile_accept-"].update(disabled=False)
     
@@ -84,19 +84,19 @@ def add_profile(url):
         window["-add_profile_state-"].update("Failed")
         window["-add_profile_accept-"].update(disabled=False)
         return
-    dl = threading.Thread(target=add_profile_thread, args=(r_url, r_id, "-add_profile_state-"),daemon=True)
+    dl = threading.Thread(target=add_profile_thread, args=(r_url, r_id),daemon=True)
     dl.start()
 
-def update_thread(url, r_id, key, old_mods_list, info_url, r_name, r_version):
+def update_thread(url, r_id, old_mods_list, info_url, r_name, r_version):
     global update_mods
     update_mods = []
     if exists(f"./profiles/{r_id}/mods.zip"):
         remove(f"./profiles/{r_id}/mods.zip")
-    window[key].update("Downloading...")
+    window["-update_state-"].update("Downloading...")
     download_file(url, f"./profiles/{r_id}/mods.zip")
-    window[key].update("Making Directory...")
+    window["-update_state-"].update("Making Directory...")
     mkdir(f"./profiles/{r_id}/temp")
-    window[key].update("Unpacking...")
+    window["-update_state-"].update("Unpacking...")
     unpack_file(f"./profiles/{r_id}/mods.zip", f"./profiles/{r_id}/temp")
     new_mods_list = set([basename(i) for i in glob.glob(f"./profiles/{r_id}/temp/*")])
     add_mods = new_mods_list - old_mods_list
@@ -110,18 +110,18 @@ def update_thread(url, r_id, key, old_mods_list, info_url, r_name, r_version):
     
     shutil.rmtree(f"./profiles/{r_id}/temp")
 
-    window[key].update("Updating infomation...")
+    window["-update_state-"].update("Updating infomation...")
     w_data = "\n".join([r_id, r_name, info_url, r_version])
     with open(f"./profiles/{r_id}/info.txt", "w") as f:
             f.write(w_data)
     
-    window[key].update("Profiles Reloading...")
+    window["-update_state-"].update("Profiles Reloading...")
     load_profiles()
     window["-profile_name-"].update(profiles_dict[combo_id]["name"])
     window["-profile_id-"].update(combo_id)
     window["-profile_version-"].update(profiles_dict[combo_id]["version"])
     window["-update_check-"].update(disabled = False)
-    window[key].update("Success!")
+    window["-update_state-"].update("Success!")
 
 
 def check_update(id):
@@ -133,13 +133,35 @@ def check_update(id):
         if float(o_version) < float(r_version):
             window["-update_state-"].update("更新があります")
             old_mods_list = set([basename(i) for i in glob.glob(f"{directory_path}/{r_id}/mods/*")])
-            dl = threading.Thread(target=update_thread, args=(r_url, r_id, "-update_state-", old_mods_list, o_url, r_name, r_version))
+            dl = threading.Thread(target=update_thread, args=(r_url, r_id, old_mods_list, o_url, r_name, r_version))
             dl.start()
         else:
             window["-update_state-"].update("最新の状態です")
     except:
         window["-update_state-"].update("Failed")
     window["-update_check-"].update(disabled=False)
+
+def delete_profile_thread(id):
+    window["-delete_state-"].update("Deleting profiles...")
+    print(f"{directory_path}/{id}")
+    print(f"./profiles/{id}")
+    shutil.rmtree(f"{directory_path}/{id}")
+    shutil.rmtree(f"./profiles/{id}")
+
+    window["-delete_state-"].update("Profiles Reloading...")
+    load_profiles()
+    window["-profile_name-"].update("")
+    window["-profile_id-"].update("")
+    window["-profile_version-"].update("")
+    window["-delete_state-"].update("Success!")
+    window["-delete_profile-"].update(disabled=False)
+
+def delete_profile():
+    if sg.popup_yes_no("本当に削除しますか？", title="") == "Yes":
+        del_thread = threading.Thread(target=delete_profile_thread, args=(combo_id,))
+        del_thread.start()
+    else:
+        window["-delete_profile-"].update(disabled=False)
 
 def main():
     global window, directory_path, selected_profile, combo_id
@@ -167,6 +189,7 @@ def main():
                             [sg.Text("ID:",size=6), sg.Text(pl_id, key="-profile_id-")],
                             [sg.Text("Version:",size=6), sg.Text(pl_version, key="-profile_version-")],
                             [sg.Button("更新をチェック", key="-update_check-", disabled=False), sg.Text("", key="-update_state-")],
+                            [sg.Button("この起動構成を削除", key="-delete_profile-"), sg.Text("", key="-delete_state-")],
                             [sg.Multiline("",size=(400, 300), key="-update_mods_list-")]]
 
     add_profile_tab = [[sg.Text("URL")],
@@ -205,6 +228,10 @@ def main():
             combo_disable_flag = False
             window["-update_check-"].update(disabled = False)
 
+        elif event == "-delete_profile-":
+            window["-delete_profile-"].update(disabled=True)
+            delete_profile()
+            
 
         elif event == "-add_profile_url_paste-":
             window["-add_profile_url-"].update(pyperclip.paste())
